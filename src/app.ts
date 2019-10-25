bd636e67507b896c6e688212c37c8907acd3d452
Stanley Huang <huang.stanley050@gmail.com>
2019-10-26 10:07:53 +1100

    export app to server.ts

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-console.log("hi");
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.get("/api", (req: Request, res: Response) => {
+  res.send("Hi there");
+});
+
+export default app;
